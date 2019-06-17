2e52c1b141cffe94cf9d5eb9f909498f5846eb15
dixon28 <33327336+dixon28@users.noreply.github.com>
2019-06-17 03:04:01 -0600

    grabacion de sigue practicando

--- playing2learn/app/src/main/java/com/pdm/sube/cum/estadisticas/EstadisticasActivity.java
+++ playing2learn/app/src/main/java/com/pdm/sube/cum/estadisticas/EstadisticasActivity.java
@@ -2,6 +2,7 @@ package com.pdm.sube.cum.estadisticas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.Description;
@@ -26,7 +27,11 @@ public class EstadisticasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas2);
+        setTitle("Estadisticas del mes");
         barChart=findViewById(R.id.grafico);
+
+
+
         Estadisticas estadisticas1= SQLite.select().from(Estadisticas.class).where(Estadisticas_Table.seccion_id.eq(1)).and(Estadisticas_Table.usuario_id.eq(getIntent().getExtras().getInt("usuario"))).and(Estadisticas_Table.mes.eq(fecha.getMonth()+1)).querySingle();
 
         Estadisticas estadisticas2= SQLite.select().from(Estadisticas.class).where(Estadisticas_Table.seccion_id.eq(2)).and(Estadisticas_Table.usuario_id.eq(getIntent().getExtras().getInt("usuario"))).and(Estadisticas_Table.mes.eq(fecha.getMonth()+1)).querySingle();
@@ -34,29 +39,41 @@ public class EstadisticasActivity extends AppCompatActivity {
 
         Estadisticas estadisticas3= SQLite.select().from(Estadisticas.class).where(Estadisticas_Table.seccion_id.eq(3)).and(Estadisticas_Table.usuario_id.eq(getIntent().getExtras().getInt("usuario"))).and(Estadisticas_Table.mes.eq(fecha.getMonth()+1)).querySingle();
 
-
-        ArrayList<BarEntry> barEntries= new ArrayList<>();
-
-        barEntries.add(new BarEntry(0,Float.parseFloat(String.valueOf(estadisticas1.getNumero_veces()))));
-
-        barEntries.add(new BarEntry(1,Float.parseFloat(String.valueOf(estadisticas2.getNumero_veces()))));
-
-        barEntries.add(new BarEntry(2,Float.parseFloat(String.valueOf(estadisticas3.getNumero_veces()))));
-
-        ArrayList<String> secciones= new ArrayList<>();
-        secciones.add("Alfabeto");
-        secciones.add("Numeros");
-        secciones.add("Mama y Papa");
-        BarDataSet barDataSet= new BarDataSet(barEntries,"secciones: Alfabeto,numeros,mama y papa");
-
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        try {
 
 
-        BarData theData= new BarData(barDataSet);
-        barChart.setData(theData);
-        barChart.setTouchEnabled(true);
-        barChart.setDragEnabled(true);
-        barChart.setScaleEnabled(true);
+            ArrayList<BarEntry> barEntries = new ArrayList<>();
+
+            barEntries.add(new BarEntry(0, Float.parseFloat(String.valueOf(estadisticas1.getNumero_veces()))));
+
+            barEntries.add(new BarEntry(1, Float.parseFloat(String.valueOf(estadisticas2.getNumero_veces()))));
+
+            barEntries.add(new BarEntry(2, Float.parseFloat(String.valueOf(estadisticas3.getNumero_veces()))));
+
+            ArrayList<String> secciones = new ArrayList<>();
+            secciones.add("Alfabeto");
+            secciones.add("Numeros");
+            secciones.add("Mama y Papa");
+            BarDataSet barDataSet = new BarDataSet(barEntries, "secciones: Alfabeto,numeros,mama y papa");
+
+            barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+
+            BarData theData = new BarData(barDataSet);
+            barChart.setData(theData);
+            barChart.setTouchEnabled(true);
+            barChart.setDragEnabled(true);
+            barChart.setScaleEnabled(true);
+            Description description =new Description();
+            description.setText("secciones mas visitidas por este mes");
+            barChart.setDescription(description);
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+
+
+        }
 
 
 
